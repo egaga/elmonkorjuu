3435f34d05e4a44fc1e37a40082c5087b3f46af6
Henrik Huttunen <henrik.huttunen@gmail.com>
2016-05-15 13:35:21 +0300

    Relocate update logic to update.elm from game.elm

--- game.elm
+++ game.elm
@@ -22,6 +22,7 @@ import UI as PlayerAction exposing (PlayerAction)
 import Random exposing (initialSeed)
 import Array exposing (..)
 import Time exposing (Time, second)
+import Update exposing (update)
 import Task
 
 -- TODO get seed from mousemovements&time
@@ -40,78 +41,6 @@ initialModel =
       deck = deck,
       discard = Array.empty }
 
-updatePlayer : Player -> Array Player -> Array Player
-updatePlayer player players =
-  updateElement players player (\(i, p)  -> p.nick == player.nick)
-
-updatePlayers : Array Player -> Array Player -> Array Player
-updatePlayers players updatedPlayers =
-  Array.foldl updatePlayer players updatedPlayers
-
-updateWith : Model -> Player -> Model
-updateWith model player =
-  { model | players = updatePlayer player model.players }
-
-noEffect : Model -> (Model, Cmd Msg)
-noEffect model =
-  (model, Cmd.none)
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-  case msg of
-    Msg.GetTime newTime ->
-      let
-        newModel = {
-          model |
-            startTime = (Just (Maybe.withDefault newTime model.startTime)),
-            currentTime = newTime }
-      in
-        (newModel, Cmd.none)
-    Msg.PlayerAction playerAction ->
-      noEffect <| updatePlayerAction playerAction model
-
-updatePlayerAction : Msg.PlayerAction -> Model -> Model
-updatePlayerAction action model =
-  case action of
-    PlayerAction.DrawCardsToTrade playerInput ->
-      let
-        (deck, player) = Domain.drawCardsToTrade model.deck playerInput
-      in
-        { model | deck = deck, players = updatePlayer player model.players }
-    PlayerAction.PlantFromHand playerInput ->
-      updateWith model (Domain.plantTopmostCard playerInput)
-    PlayerAction.PlantFromSide playerInput index ->
-      updateWith model (Domain.plantFromSide playerInput index)
-    PlayerAction.SellField playerInput index ->
-      case Domain.playerSellsField playerInput index of
-        Nothing ->
-          model
-        Just ({amount, card}, player) ->
-          let
-            newDiscard = Array.append model.discard (Array.fromList <| List.repeat amount card)
-          in
-            { model |
-              players = updatePlayer player model.players,
-              discard = newDiscard }
-    PlayerAction.DrawCardsToHand playerInput ->
-      let
-        (deck, player) = Domain.drawCardsToHand model.deck playerInput
-      in
-        { model |
-            players = updatePlayer player model.players,
-            deck = deck }
-    PlayerAction.KeepFromTrade playerInput i ->
-      updateWith model (Domain.keepFromTrade i playerInput)
-    PlayerAction.TradeFromHand fromPlayerInput i toPlayerInput ->
-      let
-        (fromPlayer, toPlayer) = Domain.tradeFromHand fromPlayerInput i toPlayerInput
-      in
-        { model | players = updatePlayers model.players (Array.fromList [fromPlayer, toPlayer]) }
-    PlayerAction.Trade fromPlayerInput i toPlayerInput ->
-      let
-        (fromPlayer, toPlayer) = Domain.trade fromPlayerInput i toPlayerInput
-      in
-        { model | players = updatePlayers model.players (Array.fromList [fromPlayer, toPlayer]) }
 
 init : (Model, Cmd Msg)
 init = (initialModel, Cmd.none)
@@ -129,5 +58,5 @@ main =
   HtmlApp.program
     { init = init,
       view = view,
-      update = update,
+      update = Update.update,
       subscriptions = subscriptions }
---
+++ update.elm
@@ -0,0 +1,81 @@
+module Update exposing (update)
+
+import Domain exposing (..)
+import UI as Msg exposing (Msg)
+import UI as PlayerAction exposing (PlayerAction)
+import Array exposing (..)
+import Time exposing (Time, second)
+import Util exposing (updateElement)
+
+updatePlayer : Player -> Array Player -> Array Player
+updatePlayer player players =
+  updateElement players player (\(i, p)  -> p.nick == player.nick)
+
+updatePlayers : Array Player -> Array Player -> Array Player
+updatePlayers players updatedPlayers =
+  Array.foldl updatePlayer players updatedPlayers
+
+updateWith : Model -> Player -> Model
+updateWith model player =
+  { model | players = updatePlayer player model.players }
+
+noEffect : Model -> (Model, Cmd Msg)
+noEffect model =
+  (model, Cmd.none)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Msg.GetTime newTime ->
+      let
+        newModel = {
+          model |
+            startTime = (Just (Maybe.withDefault newTime model.startTime)),
+            currentTime = newTime }
+      in
+        (newModel, Cmd.none)
+    Msg.PlayerAction playerAction ->
+      noEffect <| updatePlayerAction playerAction model
+
+updatePlayerAction : Msg.PlayerAction -> Model -> Model
+updatePlayerAction action model =
+  case action of
+    PlayerAction.DrawCardsToTrade playerInput ->
+      let
+        (deck, player) = Domain.drawCardsToTrade model.deck playerInput
+      in
+        { model | deck = deck, players = updatePlayer player model.players }
+    PlayerAction.PlantFromHand playerInput ->
+      updateWith model (Domain.plantTopmostCard playerInput)
+    PlayerAction.PlantFromSide playerInput index ->
+      updateWith model (Domain.plantFromSide playerInput index)
+    PlayerAction.SellField playerInput index ->
+      case Domain.playerSellsField playerInput index of
+        Nothing ->
+          model
+        Just ({amount, card}, player) ->
+          let
+            newDiscard = Array.append model.discard (Array.fromList <| List.repeat amount card)
+          in
+            { model |
+              players = updatePlayer player model.players,
+              discard = newDiscard }
+    PlayerAction.DrawCardsToHand playerInput ->
+      let
+        (deck, player) = Domain.drawCardsToHand model.deck playerInput
+      in
+        { model |
+            players = updatePlayer player model.players,
+            deck = deck }
+    PlayerAction.KeepFromTrade playerInput i ->
+      updateWith model (Domain.keepFromTrade i playerInput)
+    PlayerAction.TradeFromHand fromPlayerInput i toPlayerInput ->
+      let
+        (fromPlayer, toPlayer) = Domain.tradeFromHand fromPlayerInput i toPlayerInput
+      in
+        { model | players = updatePlayers model.players (Array.fromList [fromPlayer, toPlayer]) }
+    PlayerAction.Trade fromPlayerInput i toPlayerInput ->
+      let
+        (fromPlayer, toPlayer) = Domain.trade fromPlayerInput i toPlayerInput
+      in
+        { model | players = updatePlayers model.players (Array.fromList [fromPlayer, toPlayer]) }
